94c1a79974fad8a71bdff39983da4608b9683c2e
Alex Martinez <marty@Alexs-MacBook-Air.local>
2025-02-15 18:57:55 -0800

    Updated dropdown menu text to black

--- app/page.tsx
+++ app/page.tsx
@@ -38,8 +38,8 @@ export default function Home() {
           {menuOpen && (
             <div className="absolute right-0 mt-2 w-40 bg-white border rounded-lg shadow-lg">
               <ul className="flex flex-col">
-                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">Sign Up</li>
-                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">Login</li>
+                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer text-black">Sign Up</li>
+                <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer text-black">Login</li>
               </ul>
             </div>
           )}
